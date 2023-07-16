015f60067888fc36bfc3a44aac6d45ebd0fb0db5
jvorndran <135787855+jvorndran@users.noreply.github.com>
2023-07-16 13:52:23 -0400

    Styling

--- website/static/js/screener_script.js
+++ website/static/js/screener_script.js
@@ -118,6 +118,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+
+
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el => observer.observe(el)));
 
 });
 
