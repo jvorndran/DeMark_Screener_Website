8a3a67e72ba0c6975f220449f04e0a3c5030ccc5
jvorndran <135787855+jvorndran@users.noreply.github.com>
2023-09-28 21:13:55 -0400

    deployment

--- website/__init__.py
+++ website/__init__.py
@@ -2,11 +2,18 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.debug = True
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.debug = True
+
+    return app
+
+
+app = create_app()
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
